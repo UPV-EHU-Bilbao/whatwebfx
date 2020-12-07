5a0baffc74345ef6ccf3b9f3115b5b51853d8818
josuf6 <josuf6@gmail.com>
2020-12-07 20:37:04 +0100

    Erabiliko den datu-basea

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,27 @@
-CREATE TABLE plugins (plugin_id int NOT NULL AUTO_INCREMENT, name varchar(255) NOT NULL,PRIMARY KEY (plugin_id), UNIQUE (name));
-CREATE TABLE targets (target_id int NOT NULL AUTO_INCREMENT, target varchar(255) NOT NULL, status varchar(10),PRIMARY KEY (target_id), UNIQUE (target, status) );
-CREATE TABLE scans (scan_id int NOT NULL AUTO_INCREMENT, config_id INT NOT NULL, plugin_id INT NOT NULL, target_id INT NOT NULL, version varchar(255), os varchar(255), string varchar(1024), account varchar(1024), model varchar(1024), firmware varchar(1024), module varchar(1024), filepath varchar(1024), certainty varchar(10) ,PRIMARY KEY (scan_id));
-CREATE TABLE request_configs (config_id int NOT NULL AUTO_INCREMENT, value TEXT NOT NULL, PRIMARY KEY (config_id) );
+CREATE TABLE "plugins" (
+                           "plugin_id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                           "name"	TEXT NOT NULL UNIQUE
+);
+CREATE TABLE "targets" (
+                           "target_id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                           "target"	TEXT NOT NULL UNIQUE,
+                           "status"	TEXT UNIQUE
+);
+CREATE TABLE "scans" (
+                         "scan_id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                         "config_id"	INTEGER NOT NULL,
+                         "plugin_id"	INTEGER NOT NULL,
+                         "target_id"	INTEGER NOT NULL,
+                         "version"	TEXT,
+                         "os"	TEXT,
+                         "string"	TEXT,
+                         "model"	TEXT,
+                         "firmware"	TEXT,
+                         "module"	TEXT,
+                         "filepath"	TEXT,
+                         "certainty"	TEXT
+);
+CREATE TABLE "request_configs" (
+                                   "config_id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                                   "value"	TEXT NOT NULL
+);
