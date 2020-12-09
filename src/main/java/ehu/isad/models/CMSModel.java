26338be629ce472a1daa16367927cdec78f8b3a9
josuf6 <josuf6@gmail.com>
2020-12-09 17:14:16 +0100

    CMSModel klasea hasierako bertsioa

---
+++ src/main/java/ehu/isad/models/CMSModel.java
@@ -0,0 +1,16 @@
+package ehu.isad.models;
+
+public class CMSModel {
+
+    private final String url;
+    private final String cms;
+    private final String version;
+    private String lastUpdated;
+
+    public CMSModel(String pUrl, String pCms, String pVersion, String pLastUpdated) {
+        this.url = pUrl;
+        this.cms = pCms;
+        this.version = pVersion;
+        this.lastUpdated = pLastUpdated;
+    }
+}
