0e61b17b0560e5c88ddf23efe178e3626984e2bc
josuf6 <josuf6@gmail.com>
2020-12-11 10:42:24 +0100

    scans taulan account zutabea falta zen

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -16,6 +16,7 @@ CREATE TABLE "scans" (
                          "version"	TEXT,
                          "os"	TEXT,
                          "string"	TEXT,
+                         "account"	TEXT,
                          "model"	TEXT,
                          "firmware"	TEXT,
                          "module"	TEXT,
